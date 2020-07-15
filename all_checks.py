2e82b21da153bebb7e2003523393b99fd01e3b58
Euhid Aman <aman.derax20@gmail.com>
2020-07-15 12:17:23 +0530

    Add check_disk_full(), Rename min_absolute to min_gb, use parameter names

--- all_checks.py
+++ all_checks.py
@@ -5,11 +5,26 @@ def check_reboot():
     """returns True, if a computer has a pending reboot"""
     return os.path.exists("run/reboot-required")
 
+def check_disk_full(disk,min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
+    du=shutil.disk_usage(disk)
+    #Calculate the percentage of free space
+    percent_free = 100*du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    print("Everything Ok.")
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
+        print("Disk full.")
+        sys.exit(1)
+
+    print("Everything ok.")
     sys.exit(0)
 
 main()
